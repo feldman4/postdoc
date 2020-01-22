432fb5005b9c8774bc7d7eb556c4e89de79dbe6a
feldman4 <feldman4@users.noreply.github.com>
2020-01-21 18:14:06 -0800

    pthr

--- constants.py
+++ constants.py
@@ -5,3 +5,30 @@ GO_SYMBOL = 'DB Object Symbol'
 GO_TERM_COUNTS = 'GO_term_counts'
 SEARCH_KEYWORD = 'keyword'
 GO_SYNONYM = 'DB Object Synonym (|Synonym)'
+
+GENE_ID = 'gene_id'
+GENE_SYMBOL = 'gene_symbol'
+
+HGNC = 'HGNC'
+UNIPROTKB = 'UniProtKB'
+ENSG = 'ENSG'
+
+biomart_columns = {'Gene stable ID': ENSG,
+                   'HGNC ID': HGNC,
+                   'NCBI gene ID': GENE_ID,
+                   'HGNC symbol': GENE_SYMBOL,
+                  }
+
+PTHR = 'PTHR'
+PTHR_SF = 'PTHR_SF'
+PTHR_FAMILY = 'PTHR_family'
+PTHR_SUBFAMILY = 'PTHR_subfamily'
+PTHR_CLASS_LIST = 'PC_list'
+
+pthr_columns = {
+    0: 'identifier',
+    2: PTHR_SF,
+    3: PTHR_FAMILY,
+    4: PTHR_SUBFAMILY,
+    8: PTHR_CLASS_LIST,
+}
--- reporters.py
+++ reporters.py
@@ -13,6 +13,7 @@ goa_header = 'goa_header.csv'
 go_obo_remote = 'http://purl.obolibrary.org/obo/go/go-basic.obo'
 go_obo = os.path.basename(go_obo_remote)
 
+
 def locate_match(search, col):
     search = search.lower()
     def locate(df):
@@ -63,4 +64,75 @@ def download_GO_files():
         (pd.read_html(goa_header_remote)[0]
          .to_csv(goa_header, index=None))
 
+
+def load_biomart_genes():
+    A = (pd.read_csv('tables/ENSG_HGNC_gene-id.txt', sep='\t')
+     .rename(columns=biomart_columns)
+    )
+
+    B = (pd.read_csv('tables/HGNC_symbol.txt', sep='\t')
+     .rename(columns=biomart_columns)
+     .dropna()
+    )
+
+    return A.merge(B)
+
+
+def load_go_terms():
+    go_definitions = parse_go_obo(go_obo)
+    goa_columns = pd.read_csv(goa_header)['Content'].to_dict()
+
+    return (pd.read_csv(go_gaf, sep='\t', comment='!', 
+                         header=None, low_memory=False)
+     .rename(columns=goa_columns)
+     .assign(**{GO_TERM: lambda x: x[GO].map(go_definitions)})
+     .pipe(add_term_counts)
+    )
+
+def load_pthr(df_biomart, drop_hgnc_ensg=True, clean_symbols=True):
     
+    df_pthr = pd.read_csv('PTHR14.1_human_', sep='\t', header=None)
+    gene_identifier = df_pthr[0]
+    
+    hgnc = (gene_identifier
+     .str.extract('HUMAN\|HGNC=(\d+)\|UniProtKB=(\w+)')
+     .assign(identifier=gene_identifier)
+     .dropna()
+     .rename(columns={0: HGNC, 1: UNIPROTKB})
+     .assign(**{HGNC: lambda x: 'HGNC:' + x[HGNC].astype(str)})
+     .join(df_biomart.set_index(HGNC), on=HGNC)
+     .set_index('identifier')
+    )
+
+    ensg = (gene_identifier
+     .str.extract('HUMAN\|Ensembl=(ENSG\d+)\|UniProtKB=(\w+)')
+     .assign(identifier=gene_identifier)
+     .dropna()
+     .rename(columns={0: ENSG, 1: UNIPROTKB})
+     .join(df_biomart.set_index(ENSG), on=ENSG)
+     .set_index('identifier')
+    )
+
+    gene_ids = pd.concat([hgnc, ensg], sort=True)
+    
+    df_pthr = (df_pthr
+     .rename(columns=pthr_columns)
+     [pthr_columns.values()]
+     .assign(**{PTHR: lambda x: x[PTHR_SF].str.split(':').str[0]})
+     .join(gene_ids, on='identifier')
+     .drop('identifier', axis=1)
+     .drop_duplicates([GENE_SYMBOL, GENE_ID, PTHR_SF])
+    )
+
+    if drop_hgnc_ensg:
+        df_pthr = df_pthr.drop([HGNC, ENSG], axis=1)
+
+    if clean_symbols:
+        df_pthr = df_pthr.dropna(subset=[GENE_SYMBOL])
+
+    return df_pthr
+
+
+
+
+
