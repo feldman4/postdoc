fbcbcb1260eb8769831690e815ca4e528265db87
David Feldman <dfeldman@uw.edu>
2020-05-03 10:09:27 -0700

    imports_ipython

--- imports.py
+++ imports.py
@@ -18,20 +18,3 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-
-import IPython
-IPython.get_ipython().run_line_magic('load_ext', 'autoreload')
-IPython.get_ipython().run_line_magic('autoreload', '2')
-
-# increase dpi without increasing displayed image size
-(IPython.get_ipython()
-    .run_line_magic('config', "InlineBackend.figure_format = 'retina'"))
-
-import warnings
-with warnings.catch_warnings():
-    warnings.filterwarnings('ignore', '', category=FutureWarning)
-    import tqdm.notebook
-    tqdm.notebook.tqdm.pandas()
-
-import postdoc.helpers
-df_aa, aa_legend = postdoc.helpers.load_aa_legend()
