deaa80bebf65f31eda3f53147520f6bb5886c1e4
David Feldman <dfeldman@uw.edu>
2020-07-02 20:56:09 -0700

    collect ml imports

---
+++ wfc/imports.py
@@ -0,0 +1,14 @@
+from sklearn.linear_model import Lasso, LassoCV, LogisticRegressionCV
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.metrics import roc_curve, auc
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.svm import SVR, SVC
+
+import tensorflow as tf
+from tensorflow.keras.layers import Conv1D, Dense, Input, Softmax, Reshape
+from tensorflow.keras import Model, Sequential
+from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
+from tensorflow.keras.utils import plot_model
+from sklearn.model_selection import cross_val_score, StratifiedKFold
+
