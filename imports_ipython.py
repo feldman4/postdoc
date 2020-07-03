fa07c38b8a7b76568cb54b4af89dd5e4ae085ea7
David Feldman <dfeldman@uw.edu>
2020-07-02 20:54:41 -0700

    add sketchy format syntax

--- imports_ipython.py
+++ imports_ipython.py
@@ -36,16 +36,27 @@ aa_3_1 = df_aa.set_index('res_name')['Letter'].to_dict()
 aa_1_3 = {v: k for k, v in aa_3_1.items()}
 
 
-# allow eval shorthand with bytes string
-# df.assign(c=b'a+b') => df.assign(c=lambda x: x.eval('a+b'))
+
 def patch_assign(cls):
+    """
+    Allow eval shorthand with bytes string.
+    df.assign(c=b'a+b') => df.assign(c=lambda x: x.eval('a+b'))
+    
+    Instead of @ use {}.
+    d = 1
+    df.assign(c=b'a+b+{d}') => df.assign(c=lambda x: x.eval('a+b+@d'))
+    """
     if hasattr(cls, '_assign'):
         raise Exception('cannot redefine assign twice')
 
     def assign_bytes(self, **kwargs):
+        import inspect
+        frame = inspect.currentframe()    
+        f_locals = frame.f_back.f_locals
         for k, v in kwargs.items():
             if isinstance(v, bytes):
-                kwargs[k] = lambda x: x.eval(v.decode('ascii'))
+                kwargs[k] = lambda x: x.eval(v.decode('ascii').format(**f_locals))
+        del frame
         return cls._assign(self, **kwargs)
     cls._assign = cls.assign
     cls.assign = assign_bytes
