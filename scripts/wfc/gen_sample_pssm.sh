8cb05974ac00e5e30183bf644b20e925989f4ef7
David Feldman <dfeldman@uw.edu>
2020-07-18 13:29:25 -0700

    TrR scripts

---
+++ scripts/wfc/gen_sample_pssm.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH -p gpu
+#SBATCH --gres=gpu
+#SBATCH -c 4
+#SBATCH --mem=16g
+#SBATCH -o logs/sbatch_%A.out
+#SBATCH -e logs/sbatch_%A.out
+
+PREFIX="wfc/gen/sample_pssm/x"
+
+L=100
+N=30
+FLAGS="--opt_sample --pssm_design"
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# $PREFIX.out.txt begins with command line invocation and options dictionary   #
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+FLAGS="$FLAGS --len=$L --num=$N"
+DEFAULTS="--rm_aa=C"
+SAVE_ARGS="--save_img --save_pdb --save_npz --scwrl"
+
+TrR_v4="python /home/krypton/projects/TrR_for_design_v4/design.py"
+CMD="$TrR_v4 $FLAGS $DEFAULTS $SAVE_ARGS --out=$PREFIX"
+LOG="$PREFIX.out.txt"
+
+source activate /software/conda/envs/tensorflow
+mkdir -p "$(dirname $PREFIX)"
+echo $CMD > $LOG
+$CMD >> $LOG
