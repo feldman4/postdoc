af0cb4253338ee44547c0ddb60ef60bc243d08b2
David Feldman <dfeldman@uw.edu>
2020-07-18 13:30:12 -0700

    lengths from command line

--- scripts/predict_bkgr3.sh
+++ scripts/predict_bkgr3.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-eval "$(conda shell.bash hook)"
+# eval "$(conda shell.bash hook)"
 conda activate tensorflow
 
 PREDICT_BKGR3="python packages/rtRosetta/predict_bkgr3/__init__.py"
-for i in $(seq 50 57 $END); 
-    do $PREDICT_BKGR3 $i wfc/bkgr_models/bkgr_$i.npz; 
+for i in "$@"; do 
+    echo "predicting $i"
+    $PREDICT_BKGR3 $i wfc/bkgr_models/bkgr_$i.npz; 
 done
