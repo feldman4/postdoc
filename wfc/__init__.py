fe3dd98b35cafa878a470b9b23ab48d3bd525292
David Feldman <dfeldman@uw.edu>
2020-08-07 16:29:28 -0700

    update

--- wfc/__init__.py
+++ wfc/__init__.py
@@ -19,21 +19,22 @@ from .v12_dual import(
     pssm_to_seq,
     align_pssms,
 )
+from .dataset import feat_to_ds, add_maxes
 
-from . import v12_tools, v4_tools
+from . import v12_tools, v4_tools, losses
+
 
 from rtRosetta.apis.v4 import split_feat, prep_input
 
-def initialize_tf():
+def initialize_tf(verbose=False):
     """
     issue with memory on RTX 2080, default tf session allocates all memory but a small
     fraction must be left unallocated
     """
     import tensorflow as tf
     import subprocess
-    nvidia_gpus = str(subprocess.check_output(["nvidia-smi", "-L"]))
-    if 'RTX 2080' in nvidia_gpus:
-        physical_devices = tf.config.experimental.list_physical_devices('GPU')
-        for physical_device in physical_devices:
-            tf.config.experimental.set_memory_growth(physical_device, True)
-
+    physical_devices = tf.config.experimental.list_physical_devices('GPU')
+    for physical_device in physical_devices:
+        tf.config.experimental.set_memory_growth(physical_device, True)
+        if verbose:
+            print(f'set memory growth for {physical_device}')
