ce7052287fed3ce6b7ad97c5e8228778f3100c3e
David Feldman <dfeldman@uw.edu>
2020-10-29 20:11:59 -0700

    update app

--- scripts/app.py
+++ scripts/app.py
@@ -1,7 +1,8 @@
-import postdoc.sequence
-import os
+import fire
 
 def update_sanger():
+    import postdoc.sequence
+    import os
     from postdoc.drive import Drive
     drive = Drive()
     df_sanger = postdoc.sequence.sanger_database(drive)
@@ -10,6 +11,37 @@ def update_sanger():
 
     print(f'Wrote {len(df_sanger)} entries to {f}')
 
+
+def calculate_overlap(filename, k, header=None, col=0, sep=','):
+    """Count shared kmers among sequences
+
+    Default assumes one sequence per line, but you can load a table with `header`, `col` and
+     `sep` parameters. 
+    
+    Examples:
+        calculate_overlap sequences.list 15
+        calculate_overlap /home/dfeldman/flycodes/pool1/input_for_wei.csv 15 --header --col="design"
+
+    :param filename: list of sequences (with default arguments) or a table with sequences in `col`
+    :param k: length of kmer for analysis
+    :param header: to load a table with headers, use --header flag
+    :param col: column name if there's a header, or zero-indexed number if not 
+        (e.g., --col=1 for second column)
+    :param sep: table separator, argument to `pandas.read_csv()`
+    """
+    from postdoc.flycodes.assembly import calculate_overlap
+    import pandas as pd
+    if header is True:
+        header = 0
+    sequences = pd.read_csv(filename, header=header, sep=sep)[col]
+
+    overlap, _ = calculate_overlap(sequences, k)
+    overlap_fraction = (overlap > 0).mean()
+    count = overlap_fraction * len(sequences)
+    print(f'On average, each sequence shares a {k}-mer with {overlap_fraction:.2%}'
+    f' of sequences ({len(sequences)} total sequences)')
+
+
+
 if __name__ == '__main__':
-    import fire
     fire.Fire()
