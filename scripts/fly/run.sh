efeb0d905fdb87014f3bf9df7abb921fa5192b93
David Feldman <dfeldman@uw.edu>
2020-11-11 19:28:41 -0800

    sbatch compatible

--- scripts/fly/run.sh
+++ scripts/fly/run.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+#SBATCH -p short
+#notSBATCH -p gpu
+#notSBATCH --gres=gpu
+#SBATCH -c 8
+#SBATCH --mem=32g
+#SBATCH -o logs/sbatch_%A.out
+#SBATCH -e logs/sbatch_%A.out
 
 source activate /home/dfeldman/.conda/envs/prosit5
 
