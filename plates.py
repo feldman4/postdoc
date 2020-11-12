758d87fa1a00fb271a787b4b4b8ed15f2f97f85e
David Feldman <dfeldman@uw.edu>
2020-11-11 19:25:45 -0800

    create plates

---
+++ plates.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import string
+
+rows_96w = list('ABCDEFGH')
+cols_96w = list(range(1, 13))
+
+def standardize_well(well):
+    """Sane well labels.
+    """
+    return f'{well[0]}{int(well[1:]):02d}'
+
+wells_96w_by_col = [standardize_well(r + str(c)) for c in cols_96w for r in rows_96w]
+wells_96w_by_row = [standardize_well(r + str(c)) for r in rows_96w for c in cols_96w]
+
+def add_row_col(df, well='well', sane=False):
+    rows, cols = zip(*[well_to_row_col(w, sane=sane) for w in df[well]])
+    return df.assign(row=rows, col=cols)
+
+def well_to_row_col(well, sane=True):
+    if sane:
+        return string.ascii_uppercase.index(well[0]), int(well[1:]) - 1
+    else:
+        return well[0], int(well[1:])
+
+
+def melt_plate(df_plate):
+    df_plate = df_plate.copy()
+    df_plate.index.name = 'row'
+    df_plate.columns.name = 'col'
+    df_long = df_plate.stack().rename('value').reset_index()
+    df_long['well'] = (df_long['row'] + df_long['col'].astype(str)).apply(standardize_well)
+    
+    return df_long.set_index('well')['value']
+
