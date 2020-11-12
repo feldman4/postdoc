460c9303ceae279a657eb7612561d4e1cb69d30d
David Feldman <dfeldman@uw.edu>
2020-11-11 19:27:26 -0800

    polish snowflakes

--- flycodes/assembly.py
+++ flycodes/assembly.py
@@ -1,12 +1,13 @@
 """Pairwise assembly of oligos from pooled synthesis.
 """
 from ..sequence import reverse_complement as rc
-from ..sequence import get_kmers, translate_dna
+from ..sequence import get_kmers, translate_dna, load_codons, to_codons
 
 from collections import defaultdict
+import io
 import numpy as np
 import pandas as pd
-
+import sys
 
 orders = {
     'Agilent_070820_WeiYang':
@@ -44,6 +45,7 @@ orders = {
             'max_length': 230,
             'max_oligos': 19092,
         },
+    # good quality assembly
     'Agilent_CTLA4_WeiYang':
     {
         'oligo_file': ('/home/wyang12/Documents/Rescue/2020_July_August/'
@@ -78,9 +80,16 @@ def add_overlaps(df_agilent):
         overlap_lengths += [k]
         overlaps += [second[:k]]
         assemblies += [first + second[k:]]
+    def translate(x):
+        try:
+            return translate_dna(x)
+        except:
+            return None
     return df_agilent.assign(overlap_length=overlap_lengths, 
                              overlap=overlaps,
-                             assembly=assemblies)
+                             assembly=assemblies,
+                             assembly_aa=[translate(x) for x in assemblies]
+                             )
 
 
 def add_primers(df_agilent, outer_forward, inner_reverse, inner_forward,
@@ -96,28 +105,48 @@ def add_primers(df_agilent, outer_forward, inner_reverse, inner_forward,
            )
 
 
-
 def calculate_overlap(sequences, k):
     kmers = defaultdict(list)
 
     for i, seq in enumerate(sequences):
-        for kmer in get_kmers(seq, k):
-            kmers[kmer].append(i)
+        for j, kmer in enumerate(get_kmers(seq, k)):
+            kmers[kmer].append((i, j))
 
     n = len(sequences)
     overlap = np.zeros((n, n))
     
     for bucket in kmers.values():
-        bucket = sorted(set(bucket))
+        bucket = sorted(set([i for i, j in bucket]))
         overlap[np.ix_(bucket, bucket)] += 1
         
     overlap[np.arange(n), np.arange(n)] = 0
     return overlap, kmers
 
 
+def calculate_overlap_fraction(sequences, k):
+    """Other metrics might be better.
+    """
+    overlap, _ = calculate_overlap(sequences, k)
+    return (overlap > 0).mean()
+
+
 def GC_fraction(x):
     return (x.count('G') + x.count('C')) / len(x)
 
+
+def parse_agilent_oligos(oligos_fwd, oligos_rev, primers):
+    from Bio.SeqUtils.MeltingTemp import Tm_NN
+    cols = ['assembly_aa', 'assembly', 
+    'overlap', 'overlap_length', 'overlap_tm',
+    'primer_1', 'primer_2', 'primer_3', 'primer_4',
+    'first', 'second', 'first_insert', 'second_insert']
+    return (pd.DataFrame({'first': list(oligos_fwd), 'second': list(oligos_rev)})
+            .pipe(add_primers, **primers)
+            .pipe(add_overlaps)
+            .assign(assembly_aa=lambda x: x['assembly'].apply(translate_dna))
+            .assign(overlap_tm=lambda x: x['overlap'].apply(Tm_NN))
+            [cols]
+            )
 
 
 def load_order(name):
@@ -130,23 +159,17 @@ def load_order(name):
                          [::2].str.replace('_1st$', '').pipe(list))
         assert len(agilent_names) == len(set(agilent_names))
 
-        return (pd.DataFrame({'first': agilent_petcon[::2], 'second': agilent_petcon[1::2]})
-         .pipe(add_primers, **order['primers'])
-         .pipe(add_overlaps)
-         .assign(aa_sequence=lambda x: x['assembly'].apply(translate_dna))
-         .assign(name=agilent_names)
-        )
+        return (parse_agilent_oligos(agilent_petcon[::2], 
+                                     agilent_petcon[1::2], 
+                                     order['primers']).assign(name=agilent_names))
     if name == 'Agilent_121020_WeiYang_test':
         
         order = orders[name]
         agilent_petcon = pd.read_csv(order['oligo_file'], sep='\s+', header=None)[0].pipe(list)
 
-        return (pd.DataFrame({'first': agilent_petcon[::2], 'second': agilent_petcon[1::2]})
-         .pipe(add_primers, **order['primers'])
-         .pipe(add_overlaps)
-         .assign(aa_sequence=lambda x: x['assembly'].apply(translate_dna))
-        #  .assign(name=agilent_names)
-        )
+        return parse_agilent_oligos(agilent_petcon[::2],
+                                    agilent_petcon[1::2],
+                                    order['primers'])
     else:
         raise ValueError(name)
 
@@ -196,3 +219,115 @@ def summarize_crosstalk(df_agilent, name, filter_aa, filter_dna):
     x = df_agilent['overlap_length'].value_counts().sort_index().reset_index()
     x.columns = 'overlap_length', 'count'
     return x
+
+
+def get_allowed_swaps(organism, num_codons):
+    allowed_swaps = defaultdict(list)
+    it = (load_codons(organism)
+          .sort_values('relative_frequency', ascending=False)
+          .groupby('amino_acid')['codon_dna'])
+    for _, codons in it:
+        for codon in codons:
+            for codon_ in codons.head(num_codons):
+                allowed_swaps[codon].append(codon_)
+    return allowed_swaps
+
+
+def polish_snowflakes(sequences, k, allowed_swaps, rs, rounds=30, verbose=False):
+    starting_sequences = list(sequences)
+    sequences = list(sequences)
+    best = 1e10
+    overlap, kmers = calculate_overlap(sequences, k)
+    anneal = len(sequences)
+    if verbose:
+        log = sys.stdout
+    else:
+        log = io.StringIO()
+    for _ in range(rounds):
+        # store state
+        last_sequences = list(sequences)
+        import copy
+        last_kmers = defaultdict(list, {k: list(v) for k,v in kmers.items()})
+        altered = set()  # only modify sequences once per pass
+        
+        
+        # # make a change
+        # kmer = rs.choice(list(bad_kmers))
+        # hits = bad_kmers[kmer]
+        # for i, j in hits:
+        #     if i in altered:
+        #         continue
+        #     altered.update([i])
+        #     kmer_ = reoptimize_kmer(kmer, -j % 3, allowed_swaps, rs)
+        #     old = sequences[i]
+        #     sequences[i] = sequences[i][:j] + kmer_ + sequences[i][j + len(kmer_):]
+        #     update_kmers(kmers, old, sequences[i], i)
+        #     break
+
+        bad_kmers = [k for k, v in kmers.items() if len(v) > 1]
+        for kmer in rs.choice(bad_kmers, anneal):
+            hits = kmers[kmer]
+            for i, j in hits:
+                if i in altered:
+                    continue
+                altered.update([i])
+                kmer_ = reoptimize_kmer(kmer, -j % 3, allowed_swaps, rs)
+                old = sequences[i]
+                sequences[i] = sequences[i][:j] + kmer_ + sequences[i][j + len(kmer_):]
+                update_kmers(kmers, old, sequences[i], i)
+            
+        # check status, revert if worse
+        bad_kmers = {k: v for k, v in kmers.items() if len(v) > 1}
+        overlap_count = sum([len(x) for x in bad_kmers.values() if len(x) > 1])
+        if overlap_count > best:
+            print('reverting', file=log)
+            sequences = last_sequences
+            kmers = last_kmers
+            anneal = max(int(anneal*.7), 1) # decrease
+        else:
+            anneal = min(len(sequences), max(int(anneal * 1.3), anneal + 1)) # increase
+            print('overlaps', overlap_count, file=log)
+            print('overlapping kmers', len(bad_kmers), file=log)
+            best = overlap_count
+            last_sequences = list(sequences)
+        if len(bad_kmers) == 0:
+            break
+
+        print('changed', len(altered), 'sequences', file=log)
+
+    for s, s_ in zip(sequences, starting_sequences):
+        assert translate_dna(s) == translate_dna(s_)
+    return sequences
+
+
+def update_kmers(kmers, old, new, i):
+    k = len(next(iter(kmers)))
+    old_kmers, new_kmers = [], []
+    for kmer, kmer_ in zip(enumerate(get_kmers(old, k)), 
+                           enumerate(get_kmers(new, k))):
+        if kmer != kmer_:
+            old_kmers.append(kmer)
+            new_kmers.append(kmer_)
+
+    for j, kmer in old_kmers:
+        kmers[kmer].remove((i, j))
+    for j, kmer in new_kmers:
+        kmers[kmer].append((i, j))
+
+
+def reoptimize_kmer(kmer, frame, allowed_swaps, rs):
+    length = len(kmer) - frame
+    length -= length % 3
+    x = kmer[frame:frame + length]
+    x = ''.join([rs.choice(allowed_swaps[c]) for c in to_codons(x)])
+    return kmer[:frame] + x + kmer[frame + length:]
+    
+
+def plot_agilent_overlaps(df_agilent):
+    import seaborn as sns
+    return (df_agilent
+            .assign(overlap_tm=lambda x: np.round(x['overlap_tm']).astype(int))
+            .pivot_table(index='overlap_length', columns='overlap_tm', values='overlap', aggfunc='count')
+            .fillna(0).astype(int)
+            .pipe(sns.heatmap, annot=True, fmt='d')
+            )
