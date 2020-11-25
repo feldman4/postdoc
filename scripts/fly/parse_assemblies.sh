f7065a1d928bccd3edbd86ca0020dc88526587af
David Feldman <dfeldman@uw.edu>
2020-11-25 11:20:59 -0800

    rename

---
+++ scripts/fly/parse_assemblies.sh
@@ -0,0 +1,10 @@
+# DIR=/home/dfeldman/flycodes/assembly
+
+# echo "CTLA4_L1"
+# app.sh parse-overlap-oligos $DIR/CTLA4_L1.list
+# echo "FolateR1_L2"
+# app.sh parse-overlap-oligos $DIR/FolateR1_L2.list --dna_col=0 --name_col=None
+# echo "MS library 1"
+# app.sh parse-overlap-oligos $DIR/pool1.list --dna_col=0 --name_col=None
+# echo "MS library 2"
+# app.sh parse-overlap-oligos $DIR/pool3.list
