d4e01d75909761ea74654bddad3ae1b5acfbe4bb
David Feldman <dfeldman@uw.edu>
2020-11-25 11:21:06 -0800

    fix

--- scripts/appcompletion.sh
+++ scripts/appcompletion.sh
@@ -5,6 +5,6 @@ app.sh -- --completion \
  | sed "s/app.py/app.sh/g" \
  > ~/.fire_completion
 
-app.sh fix_fire_completion .fire_completion
+app.sh fix_fire_completion ~/.fire_completion
 
 source ~/.fire_completion
