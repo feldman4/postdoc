50f824e7034aa5d0e24ad3b53c52c0212d6d6268
David Feldman <dfeldman@uw.edu>
2020-11-25 11:19:46 -0800

    ssm analysis

---
+++ flycodes/ssm.py
@@ -0,0 +1,16 @@
+from Levenshtein import editops
+
+def add_mutations(df_ssm, design):
+    all_codes = []
+    for aa_seq in df_ssm['aa_seq']:
+        edits = editops(design, aa_seq)
+        codes = []
+        for op, i, j in edits:
+            assert op == 'replace'
+            codes += [f'{design[i]}{i+1}{aa_seq[i]}']
+        all_codes += [codes]
+    return (df_ssm
+            .assign(mutation=[','.join(x) for x in all_codes])
+            .assign(num_mutations=[len(x) for x in all_codes])
+           )
+
