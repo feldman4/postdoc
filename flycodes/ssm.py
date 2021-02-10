c01fe8f659fab47a103bb272eade20a7249e2793
David Feldman <dfeldman@uw.edu>
2021-02-09 23:02:35 -0800

    only substitutions

--- flycodes/ssm.py
+++ flycodes/ssm.py
@@ -21,3 +21,18 @@ def to_wide(df_ssm, col):
     return (df_ssm
             .pivot_table(columns='position', index='mutant', values=col, aggfunc='first')
             )
+
+
+def add_mutations2(df_ssm, design, col='aa_seq'):
+    all_codes = []
+    for aa_seq in df_ssm[col]:
+        assert len(aa_seq) == len(design)
+        codes = []
+        for i, (c1, c2) in enumerate(zip(design, aa_seq)):
+            if c1 != c2:
+                codes += [f'{c1}{i+1}{c2}']
+        all_codes += [codes]
+    return (df_ssm
+            .assign(mutation=[','.join(x) for x in all_codes])
+            .assign(num_mutations=[len(x) for x in all_codes])
+            )
