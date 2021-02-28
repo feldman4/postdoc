1cb26e92fe169a3b9b39c6acf27f32554b97660d
David Feldman <dfeldman@uw.edu>
2021-02-27 22:47:44 -0800

    control script for ntf2 rpxdock

--- scripts/ntf2/20210209.sh
+++ scripts/ntf2/20210209.sh
@@ -1,21 +1,42 @@
 #!/bin/sh
 
-SESSION=sheets_only
-LIMIT=300
 
-echo "Setting up"
-echo ./app.sh setup_files
-./app.sh setup_files
+# echo "Setting up"
+# echo ./app.sh setup_files
+# ./app.sh setup_files
 
-echo "Collecting input PDBs"
-echo ./app.sh load_monomer_table
-./app.sh load_monomer_table
+# echo "Collecting input PDBs"
+# echo ./app.sh load_monomer_table
+# ./app.sh load_monomer_table
 
-echo "Generating rpxdock commands for job submission"
-./app.sh rpxdock_all --session $SESSION --method sheets_only --make_command \
-    --limit $LIMIT > rpxdock_commands.list
 
-echo "Dumping symmetric pdbs for all results"
-echo ./app.sh dump_all_pdbs --session $SESSION --limit_docks 5
-./app.sh dump_all_pdbs --session $SESSION --limit_docks 5
+# LIMIT=300
 
+# METHOD=sheets_only
+# echo "Generating rpxdock commands for job submission"
+# ./app.sh rpxdock_all --session $METHOD --method $METHOD --make_command \
+#     --limit $LIMIT > rpxdock_commands_$METHOD.list
+
+# METHOD=sheet_helix
+# echo "Generating rpxdock commands for job submission"
+# ./app.sh rpxdock_all --session $METHOD --method $METHOD --make_command \
+#     --limit $LIMIT > rpxdock_commands_$METHOD.list
+
+
+METHOD=sheets_only
+METHOD=ss-EH_aa-ILVFM
+LIMIT=30
+
+for METHOD in ss-E_aa-ILVFM ss-EH_aa-ILVFM ss-EH_aa-ILFM ss-EH_aa-FM
+do
+    echo $METHOD
+    ./app.sh rpxdock_all --session $METHOD --method $METHOD --limit $LIMIT \
+        --make_command > sbatch/rpxdock_$METHOD.cmds
+
+    # echo "Dumping symmetric pdbs"
+    # ./app.sh dump_all_pdbs --session $METHOD --limit $LIMIT
+
+    # echo "Exporting results"
+    # ./app.sh export_results --session $METHOD
+
+done
