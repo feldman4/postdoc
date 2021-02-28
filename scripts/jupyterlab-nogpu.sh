7e962b3794edbacb901edcd816b1a42b0ec787bb
David Feldman <dfeldman@uw.edu>
2021-02-27 22:46:17 -0800

    no gpu version of jupyterlab

---
+++ scripts/jupyterlab-nogpu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -p gpu
+#SBATCH -c 4
+#SBATCH --mem=16g
+#SBATCH -o logs/jupyter_%A.out
+#SBATCH -e logs/jupyter_%A.out
+
+source activate /home/dfeldman/.conda/envs/jupyter
+
+jupyter lab --no-browser --port=5555 --ip=0.0.0.0
+
