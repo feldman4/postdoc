f9f6a5ebb1a6e04fd68c53600a0341b44a0dbdec
David Feldman <dfeldman@uw.edu>
2021-02-27 22:46:05 -0800

    setup ntf2

---
+++ scripts/ntf2/link_external.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# run from home
+
+SCRIPTS=ntf2/scripts/
+
+mkdir -p $SCRIPTS
+
+ln -s /home/norn/NTF2/201203_rocuronium/scripts/design_symmetry.py $SCRIPTS
+ln -s /home/norn/NTF2/210110_multiple_ligands_req_hbonds/master_script.py $SCRIPTS
