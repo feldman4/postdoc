f47c49d77b9fb1ca35b5f7e5d7414a59818b1741
David Feldman <dfeldman@uw.edu>
2021-10-22 08:05:18 -0700

    ms qc

---
+++ scripts/ms_qc_app.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+ENV="/home/dfeldman/.conda/envs/df-pyr-tf"
+
+if [ "$CONDA_PREFIX" != "$ENV" ]
+then
+    source activate $ENV
+fi
+
+PYTHONPATH=/home/dfeldman/packages python /home/dfeldman/packages/postdoc/lab/ms_qc_app.py "$@"
+
+<<'###EXAMPLES'
+
+# get help
+MS_APP=/home/dfeldman/ss/ms_qc_app.sh
+$MS_APP
+
+# validate a broken .raw file
+MS_APP=/home/dfeldman/ss/ms_qc_app.sh
+$MS_APP validate /projects/ms/UWPR_Exploris/Xinting/data_482/MS_500.raw
+
+# validate a working .raw file
+MS_APP=/home/dfeldman/ss/ms_qc_app.sh
+$MS_APP validate /projects/ms/UWPR_Exploris/Xinting/data_482/MS_287.raw
+
+# validate a working .raw file against a single database
+RAW_FILE=/projects/ms/UWPR_Exploris/Xinting/data_482/MS_287.raw
+DATABASE=/home/dfeldman/for/ms_qc_app/fasta/chip137_minibinders.fa
+$MS_APP validate $RAW_FILE --database=$DATABASE
+
+###EXAMPLES
