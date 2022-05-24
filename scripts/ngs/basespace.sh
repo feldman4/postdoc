1e4f9cd6fe80292489bab8d335d0cda2e362d3d0
David Feldman <dfeldman@uw.edu>
2022-05-24 11:55:41 -0700

    update

--- scripts/ngs/basespace.sh
+++ scripts/ngs/basespace.sh
@@ -8,14 +8,18 @@
 BS="/home/dfeldman/from/software/bs"
 
 LOCAL="/home/dfeldman/NGS/basespace"
+LOCAL="."
 # BIOSAMPLES="499375 499376"
 BIOSAMPLES=$@
+
+mkdir -p $LOCAL/raw
 
 for biosample in $BIOSAMPLES
 do
     bs biosample download --name $biosample -o $LOCAL/raw
     echo "Combining lanes for $biosample into $LOCAL/"$biosample"_R[12].fastq.gz"
-    touch $LOCAL/"$biosample"_R[12].fastq.gz
+    touch $LOCAL/"$biosample"_R1.fastq.gz
+    touch $LOCAL/"$biosample"_R2.fastq.gz
     chmod 700 $LOCAL/"$biosample"_R[12].fastq.gz
     ls $LOCAL/raw/$biosample*/*R1*fastq.gz | sort | xargs cat > $LOCAL/"$biosample"_R1.fastq.gz
     ls $LOCAL/raw/$biosample*/*R2*fastq.gz | sort | xargs cat > $LOCAL/"$biosample"_R2.fastq.gz
