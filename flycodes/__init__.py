4ebff12ef3f90dee9a4ccfd830605632dfa90759
David Feldman <dfeldman@uw.edu>
2023-05-11 08:47:16 -0700

    add a no DE example

--- flycodes/__init__.py
+++ flycodes/__init__.py
@@ -3,7 +3,7 @@ from . import design, designs, ms, paper
 from .design import (
     generate_peptide_set, 
     bin_by_value, 
-    load_prosit_models, 
+    load_prosit_models_cpu, 
     add_prosit,
     sort_by_spectral_efficiency, 
     snake_select_barcodes, 
